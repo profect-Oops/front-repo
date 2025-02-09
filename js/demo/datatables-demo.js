3819bedbf62001900e40274e29f007c092da1cd4
mini <mini@minibook.local>
2025-02-09 19:03:13 +0900

    [feat] 알람 설정 페이지 구현

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,4 +1,11 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+      "pageLength": 5,
+      "lengthMenu": [5, 10],
+      "language": {
+          "info": "_START_-_END_개 표시",
+          "infoEmpty": "데이터 없음"
+      }
+  });
 });
